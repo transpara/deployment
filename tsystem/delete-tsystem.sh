aad08150e8dbe1c133c5fa43b765fd8d9b11ac2b
miguel parra <miguel.parra@transpara.com>
2023-11-02 11:56:56 +0000

    wip (tsystem): add pv and pvc to tsystem-cache and change folder structure

--- tsystem/delete-tsystem.sh
+++ tsystem/delete-tsystem.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-sudo kubectl delete -f ./tsystem-api-pv.yaml
-sudo kubectl delete -f ./tsystem-api-pvc.yaml
+# Scale StatefulSets down to 1 replica
+sudo kubectl scale statefulsets tsystem-api --replicas=1
+sudo kubectl scale statefulsets tsystem-cache --replicas=1
+
+# Delete StatefulSets
 sudo kubectl delete -f ./tsystem-api.yaml
 sudo kubectl delete -f ./tsystem-cache.yaml
-sudo kubectl delete -f ./tsystem-config.yaml
+
+# Delete Deployment
 sudo kubectl delete -f ./tsystem-ui.yaml
+
+# Delete Services
+sudo kubectl delete service tsystem-api
+sudo kubectl delete service tsystem-cache
+sudo kubectl delete service tsystem-ui
+
+# Delete PVCs
+sudo kubectl delete -f ./tsystem-api-pvc.yaml
+sudo kubectl delete -f ./tsystem-cache-pvc.yaml
+
+# Delete PVs
+sudo kubectl delete -f ./tsystem-api-pv.yaml
+sudo kubectl delete -f ./tsystem-cache-pv.yaml
+
+# Delete ConfigMap
+sudo kubectl delete -f ./tsystem-config.yaml
--- tsystem/deploy-tsystem.sh
+++ tsystem/deploy-tsystem.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# Apply ConfigMap first as it might be referenced by other resources
+sudo kubectl apply -f ./tsystem-config.yaml
+
+# Apply PersistentVolumes and PersistentVolumeClaims
+# PVs and PVCs should be created before the StatefulSets that use them
 sudo kubectl apply -f ./tsystem-api-pv.yaml
 sudo kubectl apply -f ./tsystem-api-pvc.yaml
+sudo kubectl apply -f ./tsystem-cache-pv.yaml
+sudo kubectl apply -f ./tsystem-cache-pvc.yaml
+
+# Now apply the StatefulSets
+# StatefulSets may reference PVs, PVCs, and ConfigMaps
 sudo kubectl apply -f ./tsystem-api.yaml
 sudo kubectl apply -f ./tsystem-cache.yaml
-sudo kubectl apply -f ./tsystem-config.yaml
+
+# Finally, apply the Deployment
+# The Deployment may depend on the ConfigMap
 sudo kubectl apply -f ./tsystem-ui.yaml
