64a58847bf0e52b2b7bbaf3ab0aaf78870b14fb4
Miguel Parra <miguel.parra@transpara.com>
2023-12-20 17:45:25 +0000

    wip (install_k3s.sh): update script to include helm, helmfile and helm diff plugin installation

--- install_k3s.sh
+++ install_k3s.sh
@@ -33,6 +33,24 @@ export KUBECONFIG=$HOME/.kube/config
 echo "Adding KUBECONFIG to .bashrc..."
 echo "export KUBECONFIG=$HOME/.kube/config" >> $HOME/.bashrc
 
+# 2.1 - Install Helm
+echo "Installing Helm..."
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+echo "Helm installation completed. Verifying installation..."
+helm version
+
+# 2.3 - Install Helmfile
+echo "Installing Helmfile..."
+curl -fsSL -o helmfile_0.158.0_linux_amd64.tar.gz https://github.com/helmfile/helmfile/releases/download/v0.158.0/helmfile_0.158.0_linux_amd64.tar.gz
+tar -xvzf helmfile_0.158.0_linux_amd64.tar.gz -C /tmp
+sudo mv /tmp/helmfile /usr/local/bin
+echo "Helmfile installation completed. Verifying installation..."
+helmfile version
+
+# 2.4 - Install Helm Diff plugin
+echo "Installing Helm Diff plugin..."
+helm plugin install https://github.com/databus23/helm-diff
+echo "Helm Diff plugin installation completed. Verifying installation..."
+helm plugin list
+
 echo "Installation and setup complete. Please restart your shell or source ~/.bashrc to apply changes."
-
-
