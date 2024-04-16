b9ec840835a8835bfa119005684d8d5252bbe59b
diegus_cl <diegocastrolaya@gmail.com>
2024-04-16 00:05:36 +0000

    refactor: have helm charts in one place

--- tsystem/transpara-operator/update_operator.sh
+++ tsystem/transpara-operator/update_operator.sh
@@ -1,1 +1,5 @@
+cp -r ../../tsystem/helm ./helm-charts/tsystem
+cp -r ../../tstore-interface/helm ./helm-charts/tstoreinterface
+cp -r ../../tauth/helm ./helm-charts/tauth
+
 sudo make docker-build docker-push bundle-build bundle-push catalog-build catalog-push
