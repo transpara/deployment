97bd76997d77d47d134bf1d731c08491f8e7366a
diegus_cl <diegocastrolaya@gmail.com>
2024-04-16 13:39:16 +0000

    fix: generate bundle before building

--- tsystem/transpara-operator/update_operator.sh
+++ tsystem/transpara-operator/update_operator.sh
@@ -2,4 +2,4 @@ cp -r ../../tsystem/helm ./helm-charts/tsystem
 cp -r ../../tstore-interface/helm ./helm-charts/tstoreinterface
 cp -r ../../tauth/helm ./helm-charts/tauth
 
-sudo make docker-build docker-push bundle-build bundle-push catalog-build catalog-push
+sudo make docker-build docker-push bundle bundle-build bundle-push catalog-build catalog-push
