d3b673f67d81e9abcad38c1faf7ee2721cfabdc9
diegus_cl <diegocastrolaya@gmail.com>
2024-04-18 19:44:52 +0000

    feat: tsystem api accessed through /tsystem/api/v1 and tstore through /tstore

--- tsystem/transpara-operator/update_operator.sh
+++ tsystem/transpara-operator/update_operator.sh
@@ -1,5 +1,13 @@
-cp -r ../../tsystem/helm ./helm-charts/tsystem
-cp -r ../../tstore-interface/helm ./helm-charts/tstoreinterface
-cp -r ../../tauth/helm ./helm-charts/tauth
+#!/bin/bash
 
-sudo make docker-build docker-push bundle bundle-build bundle-push catalog-build catalog-push
+# Create directories and copy files for tsystem
+mkdir -p ./helm-charts/tsystem
+cp -r ../../tsystem/helm/* ./helm-charts/tsystem
+
+# Create directories and copy files for tstore-interface
+mkdir -p ./helm-charts/tstoreinterface
+cp -r ../../tstore-interface/helm/* ./helm-charts/tstoreinterface
+
+# Create directories and copy files for tauth
+mkdir -p ./helm-charts/tauth
+cp -r ../../tauth/helm/* ./helm-charts/tauth
