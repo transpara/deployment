1221951b4018216f3a11abca96825a9d51f1c7e8
dieguscl <diegocastrolaya@gmail.com>
2024-08-07 20:24:15 -0400

    feat: add opcua to operator, and working w hardcoded values

--- tsystem/transpara-operator/update_operator.sh
+++ tsystem/transpara-operator/update_operator.sh
@@ -2,24 +2,23 @@
 
 rm -rf ./helm-charts
 
-# Create directories and copy files for tsystem
+# Create directories and copy files for helm charts
 mkdir -p ./helm-charts/tsystem
 cp -r ../../tsystem/helm/* ./helm-charts/tsystem
 
-# Create directories and copy files for tstore-interface
 mkdir -p ./helm-charts/tstoreinterface
 cp -r ../../tstore-interface/helm/* ./helm-charts/tstoreinterface
 
-# Create directories and copy files for tauth
 mkdir -p ./helm-charts/tauth
 cp -r ../../tauth/helm/* ./helm-charts/tauth
 
-# Create directories and copy files for extractorodbc
 mkdir -p ./helm-charts/extractorodbc
 cp -r ../../interfaces/extractor-odbc/helm/* ./helm-charts/extractorodbc
 
-# Create directories and copy files for collectortelegraf
 mkdir -p ./helm-charts/collectortelegraf
 cp -r ../../interfaces/collector-telegraf/helm/* ./helm-charts/collectortelegraf
+
+mkdir -p ./helm-charts/collectoropcua
+cp -r ../../interfaces/collector-opcua/helm/* ./helm-charts/collectoropcua
 
 make docker-build docker-push bundle bundle-build bundle-push catalog-build catalog-push
