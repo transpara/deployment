551b278c6be028a755de9e0208cae48359a7c9d1
Miguel Parra <miguel.parra@transpara.com>
2024-09-23 07:58:30 -0500

    feat (extractor-opcua): add extractor opcua support

--- tsystem/transpara-operator/update_operator.sh
+++ tsystem/transpara-operator/update_operator.sh
@@ -12,8 +12,12 @@ cp -r ../../tstore-interface/helm/* ./helm-charts/tstoreinterface
 mkdir -p ./helm-charts/tauth
 cp -r ../../tauth/helm/* ./helm-charts/tauth
 
+# Interfaces
 mkdir -p ./helm-charts/extractorodbc
 cp -r ../../interfaces/extractor-odbc/helm/* ./helm-charts/extractorodbc
+
+mkdir -p ./helm-charts/extractoropcua
+cp -r ../../interfaces/extractor-opcua/helm/* ./helm-charts/extractoropcua
 
 mkdir -p ./helm-charts/collectortelegraf
 cp -r ../../interfaces/collector-telegraf/helm/* ./helm-charts/collectortelegraf
